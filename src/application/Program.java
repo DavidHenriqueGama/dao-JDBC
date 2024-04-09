55f1a383803b69eb34d54730700cd2ce7bd063e1
David <david90gama@yahoo.com>
2024-04-09 17:45:34 -0300

    Este commit finaliza o projeto de banco de dados com JDBC,

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,8 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.Sellerdao;
@@ -11,8 +13,7 @@ public class Program {
 
 	public static void main(String[] args) {
 		
-		;
-		
+		Scanner sc = new Scanner(System.in);
 		Sellerdao sellerDao = DaoFactory.createSellerDao();
 		
 		System.out.println("=== Teste 1: seller findbyId ===");
@@ -21,7 +22,6 @@ public class Program {
 		
 		System.out.println(seller);
 		
-		
 		System.out.println("\n=== Teste 2: seller findbyId ===");
 		Department department = new Department (2, null);
 		List<Seller> list = sellerDao.findByDepartment(department);
@@ -29,13 +29,27 @@ public class Program {
 			System.out.println(obj);
 		}
 		
-		System.out.println("\n=== Teste 3: seller findAll ===");
+		System.out.println("\n=== Teste 3: seller findbyId ===");
 		list = sellerDao.findAll();
 		for(Seller obj : list) {
 			System.out.println(obj);
 		}
 		
-		System.out.println(seller);
+		//System.out.println("\n=== Teste 4: seller insert ===");
+		//Seller newseller = new Seller(null, "Greg", "gregGmail", new Date(), 4000.0, department);
+		//sellerDao.insert(newseller);
+		//System.out.println("Inserted. New ID = " + newseller.getId());
 		
+		System.out.println("\n=== Teste 5: seller update ===");
+		seller = sellerDao.findById(1);
+		seller.setName("Marta Waine");
+		sellerDao.update(seller);
+		System.out.println("Update completed");
+		
+		System.out.println("\n=== Teste 6: seller delete ===");
+		System.out.println("Enter id for delete test: ");
+		int id = sc.nextInt();
+		sellerDao.deleteByid(id);
+		sc.close();
 	}
 }
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -26,19 +27,99 @@ public class SellerDaoJDBC implements Sellerdao {
 
 	@Override
 	public void insert(Seller obj) {
-		// TODO Auto-generated method stub
 		
+		PreparedStatement st = null;
+		try {
+			
+			st = conn.prepareStatement(
+					
+					"INSERT INTO seller "
+					+ "(Name, Email,BirthDate, BaseSalary, DepartmentId) "
+					+ "VALUES "
+					+ "(?, ?, ?, ?, ?) ", 
+					Statement.RETURN_GENERATED_KEYS);
+			
+			st.setString(1, obj.getName());
+			st.setString(2, obj.getEmail());
+			st.setDate(3, new java.sql.Date(obj.getBirthDate().getTime()));
+			st.setDouble(4, obj.getBaseSalary());
+			st.setInt(5, obj.getDepartment().getId());
+			
+			int rowsAffected = st.executeUpdate();
+			
+			if (rowsAffected > 0) {
+				
+				ResultSet rs = st.getGeneratedKeys();
+				if(rs.next()) {
+					int id = rs.getInt(1);
+					obj.setId(id);		
+				}
+				DB.closeResultSet(rs);
+			}
+			else {
+				throw new DbException("Unexpected erro! No rows affected!");
+			}	
+		}
+		catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+		}
 	}
 
 	@Override
 	public void update(Seller obj) {
-		// TODO Auto-generated method stub
+		
+		PreparedStatement st = null;
+		try {
+			
+			st = conn.prepareStatement(
+					
+					"UPDATE seller "
+					+ "SET Name = ?, Email = ?, BirthDate = ?, BaseSalary = ?, DepartmentId = ? "
+					+ "WHERE Id = ? ");
+			
+			st.setString(1, obj.getName());
+			st.setString(2, obj.getEmail());
+			st.setDate(3, new java.sql.Date(obj.getBirthDate().getTime()));
+			st.setDouble(4, obj.getBaseSalary());
+			st.setInt(5, obj.getDepartment().getId());
+			st.setInt(6, obj.getId());
+			
+			st.executeUpdate();
+				
+		}
+		catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+		}
 		
 	}
 
 	@Override
 	public void deleteByid(Integer id) {
-		// TODO Auto-generated method stub
+		PreparedStatement st = null;
+		
+		try {
+			
+			st = conn.prepareStatement("DELETE FROM seller WHERE id = ?");
+			st.setInt(1, id);
+			int rows = st.executeUpdate();
+			if(rows == 0) {
+				System.out.println("Nonexistent ID.");
+			}else {
+				System.out.println("Deleted Complete");
+			}
+		}
+		catch(SQLException e){
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+		}
 		
 	}
 	
@@ -58,6 +139,7 @@ public class SellerDaoJDBC implements Sellerdao {
 			if(rs.next()) {
 				Department dep = instantiateDepartment(rs);
 				Seller obj = instantiateSeller(rs,dep);
+				return obj;
 			}
 			return null;
 		}
@@ -100,9 +182,10 @@ public class SellerDaoJDBC implements Sellerdao {
 					+ "FROM seller INNER JOIN department "
 					+ "ON seller.DepartmentId = department.Id ");
 			rs = st.executeQuery();
-			
+
 			List<Seller> list = new ArrayList<>();
 			Map<Integer, Department> map = new HashMap<>();
+			
 			
 			while (rs.next()) {
 				
@@ -111,11 +194,8 @@ public class SellerDaoJDBC implements Sellerdao {
 				
 				if(dep == null) {
 					dep = instantiateDepartment(rs);
-					map.put(rs.getInt("DepartmentId"), dep);
-					
+					map.put(rs.getInt("DepartmentId"), dep);	
 				}
-				
-
 				Seller obj = instantiateSeller(rs,dep);
 				list.add(obj);
 			}
